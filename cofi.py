a93c9c8aa824146b556440def5127e36e492ba04
kevin C <kevin_c_us@yahoo.com>
2019-04-07 16:13:08 -0700

    edit comments and format some code lines

--- cofi.py
+++ cofi.py
@@ -20,22 +20,18 @@ plt.show()
 
 
 
-
+#simply load movie_ids.txt file, which contains mapping of movie names to ids
 import loadMovieList
 movieList = loadMovieList.Func()
                             
-#  Initialize my ratings
+#  creating my ratings for some movies in the list
 my_ratings = np.zeros((1682, 1), np.float)
 
-# Check the file movie_idx.txt for id of each movie in our dataset
-# For example, Toy Story (1995) has ID 1, so to rate it "4", you can set
+# To rate Toy Story (1995) 4 stars, which has ID 1
 my_ratings[0] = 4
 
-# Or suppose did not enjoy Silence of the Lambs (1991), you can set
+# to give Silence of the Lambs (1991) a rating of 2
 my_ratings[97] = 2
-
-# We have selected a few movies we liked / did not like and the ratings we
-# gave are as follows:
 my_ratings[6] = 3
 my_ratings[11]= 5
 my_ratings[53] = 4
@@ -56,20 +52,22 @@ Y = np.hstack((my_ratings, Y))
 R = np.hstack(((my_ratings!=0), R))
 Ymean, Ynorm = normalizeRatings.Func(Y, R)
 
-# Useful Values
 num_movies, num_users = np.shape(Y)
+#a somewhat arbitrary number.  Can be thought of as being movie genres
 num_features = 10
-X = np.random.standard_normal(num_movies * num_features) # by using standard_normal, there is no need to feature scale the parameter
+# by using standard_normal, there is no need to feature scale the parameter
+X = np.random.standard_normal(num_movies * num_features) 
 Theta = np.random.standard_normal(num_users * num_features)
+#flatten and join together X and Theta for fit fmin_cg's requirement
 initial_parameters = np.hstack([X, Theta])
 
-# Set Regularization
 lmbda = 10
 args = (Ynorm, R, num_users, num_movies, num_features, lmbda)
 from scipy import optimize
 import cofiCostFunc
 import grad
-theta = optimize.fmin_cg(cofiCostFunc.Func, initial_parameters, fprime=grad.Func, args=args, maxiter=100)
+theta = optimize.fmin_cg(cofiCostFunc.Func, initial_parameters, \
+                         fprime=grad.Func, args=args, maxiter=100)
 
 
 # Unfold the returned theta back into X and Theta
@@ -86,6 +84,7 @@ movieList = loadMovieList.Func()
 ix = np.argsort(my_predictions)
 print('Top recommendations for you:\n')
 for i in range(10):
-    print('Predicting rating ', my_predictions[ix[totalMovies[0] - 1 - i]], movieList[ix[totalMovies[0] - 1 - i]][1])
+    print('Predicting rating ', my_predictions[ix[totalMovies[0] - 1 - i]], \
+          movieList[ix[totalMovies[0] - 1 - i]][1])
     
-#print('\n\nOriginal ratings provided:\n')
+
